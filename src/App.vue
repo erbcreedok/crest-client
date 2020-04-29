a102850f151785dc1ea8fad0dd07947b18d92f74
Ербол Сыздық <ysyzdyk@alfabank.kz>
2020-04-29 20:02:49 +0600

    add version checking

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div id="app">
     <router-view/>
+    <span style="opacity: 0.3; font-size: 8px; position: fixed; bottom: 0;">
+      version: {{version}}
+    </span>
   </div>
 </template>
+
+<script>
+import { version } from '../package.json';
+
+export default {
+  data() {
+    return {
+      version,
+    };
+  },
+};
+</script>
